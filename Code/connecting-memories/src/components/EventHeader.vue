b8ed844e8b0cf523896645e766188a1509fd0c5e
nobermair <71379007+nobermair@users.noreply.github.com>
2023-12-14 11:36:55 +0100

    Update EventHeader.vue

--- Code/connecting-memories/src/components/EventHeader.vue
+++ Code/connecting-memories/src/components/EventHeader.vue
@@ -1,7 +1,26 @@
 <template>
-  <h1>Ersteller</h1>
+  <p>Veranstalter: {{ groupAdmin?.vorname }} {{ groupAdmin?.nachname }}</p>
 </template>
 
 <script>
 
+import axios from "axios";
+  export default {
+      data () {
+          return {
+              groupAdmin: null
+          }
+      },
+
+      async mounted () {
+          let id = sessionStorage.getItem("groupId")
+          await axios
+              .get("http://localhost:8080/api/eventgroup/getById/" + id)
+              .then((response) => {
+                  this.groupAdmin = response.data.groupAdmin
+              })
+
+          console.log(this.groupAdmin)
+      }
+  }
 </script>
